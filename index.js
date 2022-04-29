a636935f8c909bca6762891696d008fb6a3b88a7
Nur Uddin <nuruddin46bd@gmail.com>
2022-04-29 07:18:14 +0600

    express and cors set

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+// middleware
+app.use(cors());
+app.use(express.json);
+
+// dbUser2
+// VUGT7qvCngBNBFvi
+
+app.get("/", (req, res) => {
+  res.send("Running smartEx warehouse");
+});
+
+app.listen(port, () => {
+  console.log("Listening to port", port);
+});
